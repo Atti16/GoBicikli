a3c60df97e757f4579040c962b7a642e5ea1d965
barossfanni <bodnar.fanni.1030@dszcbaross.edu.hu>
2025-03-20 09:08:53 +0100

    asd

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const mysql = require('mysql2/promise'); // Promise alapú MySql kliens
-const bcrypt = require('bcrypt'); // Jelszó hash eléshez
+const mysql = require('mysql2/promise'); 
+const bcrypt = require('bcrypt'); 
 
 const app = express();
 
